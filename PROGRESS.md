f228ccfd8db20cf152c917e1505c8669fc018e21
AihamAnter <aiham.anter.g51249@mcast.edu.mt>
2025-11-01 13:05:24 +0000

    trying the image adding to the document

--- PROGRESS.md
+++ PROGRESS.md
@@ -1,2 +1,2 @@
 Day 1: project setup complete.
-day 2:
+day 2:![alt text](DBSS.png)
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel one</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -137,5 +137,5 @@
             </div>
         </div>
     </body>
-    s
+    
 </html>
